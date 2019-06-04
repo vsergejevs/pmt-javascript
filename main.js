b5bfd9e3eef4ed4e569d9268c88d5e98c8930bdc
Valeriy Sergeyev <valeriy.sergeyev7@gmail.com>
2019-06-04 23:07:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,42 @@
 
-let rate = 0;
-let nper = 6;
-let pv = 1000;
-let pmt;
+function pmtCalc() {
+    
+    let pv;
+    let decimalCheck;
+    let negativeCheck;
+    let rateElement;
+    let rateIndex;
+    let rate;
+    let nperElement;
+    let nperIndex;
+    let nper;
+    let pmt;
+    
+    pv = document.getElementById("presentValue").value;
+    decimalCheck = (pv - Math.floor(pv)) !== 0;
+    negativeCheck = Math.sign(Number(pv));
 
-rate = rate / 12;
+    if (pv.trim() == '' || isNaN(pv) || decimalCheck || negativeCheck == -1) {
+        alert("Lūdzu ievadiet aizņēmuma summas laukā vēlamo aizņēmumu rakstot tikai veselus pozitīvus skaitļus");
+        document.getElementById("presentValue").value = '';
+        document.getElementById("mnthlPay").innerHTML = '';
+    } 
+    else {
+        rateElement = document.getElementById("intrst");
+        rateIndex = rateElement.selectedIndex;
+        rate = Number(rateElement.options[rateIndex].value);
+        rate = rate / 12;
+    
+        nperElement = document.getElementById("prd");
+        nperIndex = nperElement.selectedIndex;
+        nper = Number(nperElement.options[nperIndex].value);
+        
+        pmt = pv * rate/(1 - (Math.pow(1 + rate, -nper)));
+        pmt = Number(pmt);
+        pmt = pmt.toFixed(2);
+    
+        document.getElementById("mnthlPay").innerHTML = pmt + " €";
+    }
+}
 
-pmt = pv * rate/(1 - (Math.pow(1 + rate, -nper)));
-pmt = Number(pmt.toFixed(2));
-
-console.log(pmt);
 
